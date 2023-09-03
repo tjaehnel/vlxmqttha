f841c6d72466a687b9e7751f9dd82fa24f4e6ad2
Tobias Jaehnel <tjaehnel@gmail.com>
2023-09-03 10:46:18 +0200

    Basic registering and moving.

---
+++ mqtt_cover.py
@@ -0,0 +1,100 @@
+"""
+this module contains all code for MQTT covers
+"""
+#  Copyright (c) 2023 - Tobias Jaehnel
+#  This code is published under the MIT license
+
+import threading
+import time
+
+from paho.mqtt.client import Client, MQTTMessage
+
+from ha_mqtt import mqtt_device_base, util
+from ha_mqtt.mqtt_device_base import MqttDeviceSettings
+
+
+class MqttCover(mqtt_device_base.MqttDeviceBase):
+    """
+    MQTT Cover class.
+    Implements a binary switch, that knows the states ON and OFF
+
+    Usage:
+    assign custom functions to the `callback_on` and `callback_off` members.
+    These functions get executed in a separate thread once the according payload was received
+
+    .. attention::
+       Each callback spawns a new thread which is automatically destroyed once the function finishes.
+       Be aware of that if you do non threadsafe stuff in your callbacks
+    """
+
+    device_type = "cover"
+    #initial_state = util.OFF
+
+    def __init__(self, settings: MqttDeviceSettings):
+        # internal tracker of the state
+        #self.state: bool = self.__class__.initial_state
+
+        # callback executed when a OPEN command is received via MQTT
+        self.callback_open = lambda: None
+        # callback executed when a CLOSE command is received via MQTT
+        self.callback_close = lambda: None
+        # callback executed when a STOP command is received via MQTT
+        self.callback_stop = lambda: None
+        # callback executed when a opening position is received via MQTT
+        self.callback_position = lambda: None
+        
+        self.command_topic = ""
+        self.position_topic = ""
+
+        super().__init__(settings)
+
+    def close(self):
+        self._client.unsubscribe(self.command_topic)
+        super().close()
+
+    def pre_discovery(self):
+        self.position_topic = f"{self.base_topic}/position"
+        self.command_topic = f"{self.base_topic}/set"
+
+        self.add_config_option("position_topic", self.position_topic)
+        self.add_config_option("command_topic", self.command_topic)
+        self.add_config_option("set_position_topic", self.command_topic)
+        self.add_config_option("position_open", 0)
+        self.add_config_option("position_closed", 100)
+
+        self._client.subscribe(self.command_topic)
+        self._client.message_callback_add(self.command_topic, self.command_callback)
+
+    def publish_position(self, position: int, retain: bool = True):
+        self._logger.debug("publishing position '%s' for %s", position, self._unique_id)
+
+        self._client.publish(self.position_topic, position, retain=retain)
+        time.sleep(0.01)
+
+
+    def command_callback(self, client: Client, userdata: object, msg: MQTTMessage):  # pylint: disable=W0613
+        """
+        callback that is executed when a message on the *command* channel is received
+
+        :param client: client who received the message
+        :param userdata: user defined data of any type that is passed as the userdata parameter to callbacks.
+          It may be updated at a later point with the user_data_set() function.
+        :param msg: actual message sent
+
+        """
+
+        self._logger.debug("Received command '%s' for %s: ", str(msg.payload), self._unique_id)
+        try:
+            if(msg.payload == b'OPEN'):
+                threading.Thread(target=self.callback_open, name="callback_thread").start()
+            elif(msg.payload == b'CLOSE'):
+                threading.Thread(target=self.callback_close, name="callback_thread").start()
+            elif(msg.payload == b'STOP'):
+                threading.Thread(target=self.callback_stop, name="callback_thread").start()
+            elif int(msg.payload) >= 0:
+                threading.Thread(target=self.callback_position, name="callback_thread", args=[int(msg.payload)]).start()
+            else:
+                self._logger.error("Unknown command '%s' for %s", str(msg.payload), self._unique_id)
+        except Exception as e:
+            self._logger.error("Exception while processing received command '%s' for %s: ", str(msg.payload), self._unique_id, e)
+ 
---
+++ vlxmqtthomeassistant.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python3
+import os
+import sys
+import signal
+import logging
+import configparser
+import paho.mqtt.client as mqtt
+import argparse
+import asyncio
+from pyvlx import Position, PyVLX, OpeningDevice
+from pyvlx.log import PYVLXLOG
+
+from ha_mqtt.ha_device import HaDevice
+from ha_mqtt.mqtt_device_base import MqttDeviceSettings
+from mqtt_cover import MqttCover
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter,
+                                 description="Allows to control devices paired with Velux KLF200 via MQTT.\n" \
+                                    "Registers the devices to Homeassistant using MQTT Autodiscovery.")
+parser.add_argument('config_file', metavar="<config_file>", help="configuration file")
+args = parser.parse_args()
+
+# read and parse config file
+config = configparser.RawConfigParser()
+config.read(args.config_file)
+# [mqtt]
+MQTT_HOST = config.get("mqtt", "host")
+MQTT_PORT = config.getint("mqtt", "port")
+MQTT_LOGIN = config.get("mqtt", "login", fallback=None)
+MQTT_PASSWORD = config.get("mqtt", "password", fallback=None)
+# [velux]
+VLX_HOST = config.get("velux", "host")
+VLX_PW = config.get("velux", "password")
+# [log]
+VERBOSE = config.get("log", "verbose")
+LOGFILE = config.get("log", "logfile", fallback=None)
+
+APPNAME = "veluxmqtthomeassistant"
+
+# init logging 
+LOGFORMAT = '%(asctime)-15s %(message)s'
+
+if VERBOSE:
+    loglevel = logging.DEBUG
+else:
+    loglevel = logging.INFO
+
+if LOGFILE:
+    logging.basicConfig(filename=LOGFILE, format=LOGFORMAT, level=loglevel)
+else:
+    logging.basicConfig(stream=sys.stdout, format=LOGFORMAT, level=loglevel)
+
+logging.info("Starting " + APPNAME)
+if VERBOSE:
+    logging.info("DEBUG MODE")
+else:
+    logging.debug("INFO MODE")
+
+PYVLXLOG.setLevel(logging.FATAL)
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.FATAL)
+PYVLXLOG.addHandler(ch)
+
+class VeluxMqttCover:
+    def __init__(self, mqttc, vlxnode, mqttid):
+        logging.debug("Registering %s to Homeassistant" % (vlxnode.name))
+        self.vlxnode = vlxnode
+        self.haDevice = HaDevice("DEV-" + vlxnode.name, "dev-" + mqttid)
+        self.coverDevice = MqttCover(MqttDeviceSettings("cover", "dev-" + mqttid + "-cover", mqttc, self.haDevice))
+
+    async def registerMqttCallbacks(self):
+        self.coverDevice.callback_open = self.mqtt_callback_open
+        self.coverDevice.callback_close = self.mqtt_callback_close
+        self.coverDevice.callback_stop = self.mqtt_callback_stop
+        self.coverDevice.callback_position = self.mqtt_callback_position
+        
+    def updateNode(self):
+        self.coverDevice.publish_position(self.vlxnode.position.position_percent)
+    
+    def mqtt_callback_open(self):
+        logging.debug("Opening %s", self.vlxnode.name)
+        asyncio.run(self.vlxnode.open(wait_for_completion=False))
+
+    def mqtt_callback_close(self):
+        logging.debug("Closing %s", self.vlxnode.name)
+        asyncio.run(self.vlxnode.close(wait_for_completion=False))
+
+    def mqtt_callback_stop(self):
+        logging.debug("Stopping %s", self.vlxnode.name)
+        asyncio.run(self.vlxnode.stop(wait_for_completion=False))
+
+    def mqtt_callback_position(self, position):
+        logging.debug("Moving %s to position %s" % (self.vlxnode.name, position))
+        asyncio.run(self.vlxnode.set_position(Position(position_percent=int(position)), wait_for_completion=False))
+
+    def __del__(self):
+        logging.debug("Unregistering %s from Homeassistant" % (self.vlxnode.name))
+        self.coverDevice.close()
+
+
+class VeluxMqttHomeassistant:
+    def __init__(self):
+        # MQTT
+        MQTT_CLIENT_ID = APPNAME + "_%d" % os.getpid()
+        self.mqttc = mqtt.Client(MQTT_CLIENT_ID)
+        self.mqttDevices = {}
+
+    async def connect_mqtt(self):
+        logging.debug("MQTT broker : %s" % MQTT_HOST)
+        if MQTT_LOGIN:
+            logging.debug("  port      : %s" % (str(MQTT_PORT)))
+            logging.debug("  login     : %s" % MQTT_LOGIN)
+
+        # set login and password, if available
+        if MQTT_LOGIN:
+            self.mqttc.username_pw_set(MQTT_LOGIN, MQTT_PASSWORD)
+
+        # Connect to the broker and enter the main loop
+        result = self.mqttc.connect(MQTT_HOST, MQTT_PORT, 60)
+        while result != 0:
+            logging.info("Connection failed with error code %s. Retrying", result)
+            await asyncio.sleep(10)
+            result = self.mqttc.connect(MQTT_HOST, MQTT_PORT, 60)
+
+        self.mqttc.loop_start()
+        await asyncio.sleep(1)
+
+    async def connect_klf200(self, loop):
+        logging.debug("klf200      : %s" % VLX_HOST)
+        self.pyvlx = PyVLX(host=VLX_HOST, password=VLX_PW, loop=loop)
+        await self.pyvlx.load_nodes()
+
+        logging.debug("vlx nodes   : %s" % (len(self.pyvlx.nodes)))
+        for node in self.pyvlx.nodes:
+            logging.debug("  %s" % node.name)
+
+    async def register_devices(self):
+        # register callbacks
+        for vlxnode in self.pyvlx.nodes:
+            if isinstance(vlxnode, OpeningDevice):
+                vlxnode.register_device_updated_cb(self.vlxnode_callback)
+                mqttid = self.generate_id(vlxnode)
+                mqttCover = VeluxMqttCover(self.mqttc, vlxnode, mqttid)
+                self.mqttDevices[mqttid] = mqttCover
+                await mqttCover.registerMqttCallbacks()
+                logging.debug("watching: %s" % vlxnode.name)
+
+    async def vlxnode_callback(self, vlxnode):
+        logging.debug("%s at %d%%" % (vlxnode.name, vlxnode.position.position_percent))
+        mqttid = self.generate_id(vlxnode)
+        mqttDevice = self.mqttDevices[mqttid]
+        if mqttDevice:
+            mqttDevice.updateNode()
+
+    def generate_id(self, vlxnode):
+        return "vlx-" + vlxnode.name.replace(" ", "-").lower()
+
+
+    def __del__(self):
+        for mqttDeviceId in self.mqttDevices:
+            del self.mqttDevices[mqttDeviceId]
+            self.mqttDevices.pop(mqttDeviceId)
+        logging.info("Disconnecting from MQTT broker")
+        self.mqttc.disconnect()
+        self.mqttc.loop_stop()
+
+        logging.info("Disconnecting from KLF200")
+        self.pyvlx.disconnect()
+
+
+# Use the signal module to handle signals
+signal.signal(signal.SIGTERM, lambda: asyncio.get_event_loop().stop())
+signal.signal(signal.SIGINT, lambda: asyncio.get_event_loop().stop())
+
+if __name__ == '__main__':
+    # pylint: disable=invalid-name
+    try:
+        LOOP = asyncio.get_event_loop()
+
+        pid = str(os.getpid())
+        pidfile = "/tmp/vlxmqtthomeassistant.pid"
+
+        if os.path.isfile(pidfile):
+            print("%s already exists, exiting" % pidfile)
+            sys.exit()
+        file = open(pidfile, 'w')
+        file.write(pid)
+        file.close()
+
+        veluxMqttHomeassistant = VeluxMqttHomeassistant()
+        LOOP.run_until_complete(veluxMqttHomeassistant.connect_mqtt())
+        LOOP.run_until_complete(veluxMqttHomeassistant.connect_klf200(LOOP))
+        LOOP.run_until_complete(veluxMqttHomeassistant.register_devices())
+
+        LOOP.run_forever()
+    except KeyboardInterrupt:
+        logging.info("Interrupted by keypress")
+    finally:
+        del veluxMqttHomeassistant
+        os.unlink(pidfile)
+    LOOP.close()
+    sys.exit(0)
